8fd2939b03b887c67f0c829592275e60ef5b9095
Nadzrul13 <khairnadzrul@gmail.com>
2023-10-29 21:55:16 +0700

    codingan konversi

---
+++ README.md
@@ -0,0 +1,7 @@
+# Program Konversi Bilangan Desimal ke Biner 
+1. user memasukkan angka desimal
+2. program memproses angka yang telah diinputkan user
+3. kemudian program mengeluarkan inputan bilangan biner dari angka desimal yang telah dimasukkan
+
+                             ~Selamat Mencoba~
+
---
+++ src/DecimalToBinary.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+/**
+ * author Nadzrul
+ *
+ * */
+public class DecimalToBinary {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan bilangan desimal: ");
+        int decimal = input.nextInt();
+        String binary = decimalToBinary(decimal);
+        System.out.println("Bilangan binernya adalah: " + binary);
+    }
+/**
+ * param decimal harus diisi dengan bilangan positif
+ * */
+    public static String decimalToBinary(int decimal) {
+        if (decimal == 0) {
+            return "0";
+        }
+
+        StringBuilder binary = new StringBuilder();
+        while (decimal > 0) {
+            int remainder = decimal % 2;
+            binary.insert(0, remainder);
+            decimal /= 2;
+        }
+
+        return binary.toString();
+    }
+}
